309413fee1aa3caa0c9e200e359da670985cc180
xoralife <abidhussainiouu@gmail.com>
2026-06-12 11:59:18 +0500

    v.6

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,14 +3,17 @@ from app.schemas.user import UserSchema
 from app.models.user import User
 from app.database import SessionLocal
 from dependencies import get_db
+
 router = APIRouter(
     prefix="/users",
 )
 
 @router.get("")
 def search(name : str="",db = depends(get_db())):
-    users = db.query(User).filter(User.name.ilike(f"%.com")).all()
-    return users
+
+    # users = db.query(User).filter(
+    #     User.name.ilike(f"%.com")).all()
+    # return users
 @router.post("")
 def store(request: UserSchema,db = depends(get_db())):
     user = db.query(User).filter(User.email == request.email).first()
@@ -27,6 +30,20 @@ def store(request: UserSchema,db = depends(get_db())):
         email=request.email
     )
     db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+@router.put("/{user_id}")
+
+def update(user_id : int, data : UserSchema,db = depends(get_db())):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user.name = data.name
+    
+    user.email = data.email
     db.commit()
     db.refresh(user)
     return user
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,4 +2,15 @@ from pydantic import BaseModel , Field
 
 class UserSchema(BaseModel):
     username: str = Field(min_length=3, max_length=50)
-    email: str
+    email: EmailStr
+    @Field_validator("email")
+    @classmethod 
+    def check_email(cls,value , db = Depends(get_db)):
+        user = db.query(User).filter(
+            User.email == value).first(
+                User.email == value
+            ).first()
+        if user:
+            raise ValueError("Email already exists")
+        return value
+
